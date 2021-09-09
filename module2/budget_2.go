a17a5c02b23865a83e9246b6b53c28cce874e146
Lakshya Nanjangud <lnanjangud@lnanjangud-a01.vmware.com>
2021-09-09 13:54:16 +0530

    changed budget

--- module2/budget_2.go
+++ module2/budget_2.go
@@ -53,7 +53,14 @@ var errDuplicateEntry = errors.New("Cannot add duplicate entry")
 
 // AddItem adds an item to the current budget
 func (b *Budget) AddItem(description string, price float32) error {
-
+	if b.CurrentCost() + price > b.Max{
+		return errors.New("errDoesNotFitBudget")
+	}
+	newItem := Item{
+		Description: description,
+		Price:       price,
+	}
+	b.Items = append(b.Items, newItem)
 	return nil
 }
 
